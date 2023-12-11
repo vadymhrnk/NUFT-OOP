7235fd48ffa466d8fb0954ddd7b2b0390fdcf9c3
Vadym Hurnik <vad.com.z@gmail.com>
2023-12-12 00:49:33 +0200

    Lab 10 basic

--- Lab10_VadymHurnik/Task1/Form1.Designer.cs
+++ Lab10_VadymHurnik/Task1/Form1.Designer.cs
@@ -1,16 +1,10 @@
-﻿namespace Task1
+﻿// MainForm.Designer.cs
+namespace ToyStoreApp
 {
-	partial class Form1
+	partial class MainForm
 	{
-		/// <summary>
-		///  Required designer variable.
-		/// </summary>
 		private System.ComponentModel.IContainer components = null;
 
-		/// <summary>
-		///  Clean up any resources being used.
-		/// </summary>
-		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
 		protected override void Dispose(bool disposing)
 		{
 			if (disposing && (components != null))
@@ -20,20 +14,107 @@
 			base.Dispose(disposing);
 		}
 
-		#region Windows Form Designer generated code
-
-		/// <summary>
-		///  Required method for Designer support - do not modify
-		///  the contents of this method with the code editor.
-		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.btnFindToys = new System.Windows.Forms.Button();
+			this.txtMaxPrice = new System.Windows.Forms.TextBox();
+			this.lblMaxPrice = new System.Windows.Forms.Label();
+			this.lblAgeGroup = new System.Windows.Forms.Label();
+			this.txtAgeGroup = new System.Windows.Forms.TextBox();
+			this.btnFindExpensiveToys = new System.Windows.Forms.Button();
+			this.btnFindToyPrices = new System.Windows.Forms.Button();
+			this.txtToyType = new System.Windows.Forms.TextBox();
+			this.lblToyType = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+
+			// btnFindToys
+			this.btnFindToys.Location = new System.Drawing.Point(12, 80);
+			this.btnFindToys.Name = "btnFindToys";
+			this.btnFindToys.Size = new System.Drawing.Size(150, 23);
+			this.btnFindToys.Text = "Find Toys";
+			this.btnFindToys.UseVisualStyleBackColor = true;
+			this.btnFindToys.Click += new System.EventHandler(this.btnFindToys_Click);
+
+			// txtMaxPrice
+			this.txtMaxPrice.Location = new System.Drawing.Point(188, 16);
+			this.txtMaxPrice.Name = "txtMaxPrice";
+			this.txtMaxPrice.Size = new System.Drawing.Size(100, 20);
+
+			// lblMaxPrice
+			this.lblMaxPrice.AutoSize = true;
+			this.lblMaxPrice.Location = new System.Drawing.Point(12, 19);
+			this.lblMaxPrice.Name = "lblMaxPrice";
+			this.lblMaxPrice.Size = new System.Drawing.Size(107, 13);
+			this.lblMaxPrice.Text = "Max Price (UAH):";
+
+			// lblAgeGroup
+			this.lblAgeGroup.AutoSize = true;
+			this.lblAgeGroup.Location = new System.Drawing.Point(12, 53);
+			this.lblAgeGroup.Name = "lblAgeGroup";
+			this.lblAgeGroup.Size = new System.Drawing.Size(99, 13);
+			this.lblAgeGroup.Text = "Age Group (e.g., 2-5):";
+
+			// txtAgeGroup
+			this.txtAgeGroup.Location = new System.Drawing.Point(188, 50);
+			this.txtAgeGroup.Name = "txtAgeGroup";
+			this.txtAgeGroup.Size = new System.Drawing.Size(100, 20);
+
+			// btnFindExpensiveToys
+			this.btnFindExpensiveToys.Location = new System.Drawing.Point(12, 119);
+			this.btnFindExpensiveToys.Name = "btnFindExpensiveToys";
+			this.btnFindExpensiveToys.Size = new System.Drawing.Size(150, 23);
+			this.btnFindExpensiveToys.Text = "Find Expensive Toys";
+			this.btnFindExpensiveToys.UseVisualStyleBackColor = true;
+			this.btnFindExpensiveToys.Click += new System.EventHandler(this.btnFindExpensiveToys_Click);
+
+			// btnFindToyPrices
+			this.btnFindToyPrices.Location = new System.Drawing.Point(12, 161);
+			this.btnFindToyPrices.Name = "btnFindToyPrices";
+			this.btnFindToyPrices.Size = new System.Drawing.Size(150, 23);
+			this.btnFindToyPrices.Text = "Find Toy Prices";
+			this.btnFindToyPrices.UseVisualStyleBackColor = true;
+			this.btnFindToyPrices.Click += new System.EventHandler(this.btnFindToyPrices_Click);
+
+			// lblToyType
+			this.lblToyType.AutoSize = true;
+			this.lblToyType.Location = new System.Drawing.Point(185, 147);
+			this.lblToyType.Name = "lblToyType";
+			this.lblToyType.Size = new System.Drawing.Size(57, 13);
+			this.lblToyType.Text = "Toy Type:";
+
+			// txtToyType
+			this.txtToyType.Location = new System.Drawing.Point(188, 161);
+			this.txtToyType.Name = "txtToyType";
+			this.txtToyType.Size = new System.Drawing.Size(100, 20);
+
+
+			// MainForm
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Text = "Form1";
+			this.ClientSize = new System.Drawing.Size(400, 220);
+			this.Controls.Add(this.lblToyType);
+			this.Controls.Add(this.txtToyType);
+			this.Controls.Add(this.btnFindToyPrices);
+			this.Controls.Add(this.btnFindExpensiveToys);
+			this.Controls.Add(this.txtAgeGroup);
+			this.Controls.Add(this.lblAgeGroup);
+			this.Controls.Add(this.lblMaxPrice);
+			this.Controls.Add(this.txtMaxPrice);
+			this.Controls.Add(this.btnFindToys);
+			this.Name = "MainForm";
+			this.Text = "Toy Store App";
+			this.ResumeLayout(false);
+			this.PerformLayout();
 		}
 
-		#endregion
+		private System.Windows.Forms.Button btnFindToys;
+		private System.Windows.Forms.TextBox txtMaxPrice;
+		private System.Windows.Forms.Label lblMaxPrice;
+		private System.Windows.Forms.Label lblAgeGroup;
+		private System.Windows.Forms.TextBox txtAgeGroup;
+		private System.Windows.Forms.Button btnFindExpensiveToys;
+		private System.Windows.Forms.Button btnFindToyPrices;
+		private System.Windows.Forms.TextBox txtToyType;
+		private System.Windows.Forms.Label lblToyType;
 	}
 }
--- Lab10_VadymHurnik/Task1/Form1.cs
+++ Lab10_VadymHurnik/Task1/Form1.cs
@@ -1,10 +1,141 @@
-namespace Task1
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace ToyStoreApp
 {
-	public partial class Form1 : Form
+	public partial class MainForm : Form
 	{
-		public Form1()
+		private List<Toy> toys;
+
+		public MainForm()
 		{
 			InitializeComponent();
+			LoadToyData();
+		}
+
+		private void LoadToyData()
+		{
+			toys = new List<Toy>();
+
+			try
+			{
+				// Assuming toy data is stored in a text file named "ToyData.txt"
+				string[] lines = File.ReadAllLines("../../../ToyData.txt");
+
+				foreach (string line in lines)
+				{
+					string[] fields = line.Split(',');
+					if (fields.Length == 3)
+					{
+						string name = fields[0].Trim();
+						string priceString = fields[1].Trim().Replace('.', ',');
+
+						// Validate and handle empty price input
+						if (string.IsNullOrEmpty(priceString))
+						{
+							MessageBox.Show("Invalid price in toy data.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+							continue;
+						}
+
+						double price = double.Parse(priceString);
+						string ageRange = fields[2].Trim();
+
+						Toy toy = new Toy(name, price, ageRange);
+						toys.Add(toy);
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show("Error loading toy data: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+			}
+		}
+
+		private void btnFindToys_Click(object sender, EventArgs e)
+		{
+			// Validate and handle empty max price or age group input
+			if (string.IsNullOrEmpty(txtMaxPrice.Text) || string.IsNullOrEmpty(txtAgeGroup.Text))
+			{
+				MessageBox.Show("Please enter valid values for Max Price and Age Group.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				return;
+			}
+
+			// 1) Find toys based on price and age range
+			double maxPrice = Convert.ToDouble(txtMaxPrice.Text);
+			string ageGroup = txtAgeGroup.Text.Trim();
+
+			var filteredToys = toys.Where(t => t.Price <= maxPrice && t.IsAgeAppropriate(ageGroup));
+
+			// Display results
+			MessageBox.Show($"Toys within the specified price range for {ageGroup}:\n\n{string.Join("\n", filteredToys)}", "Results", MessageBoxButtons.OK, MessageBoxIcon.Information);
+		}
+
+		private void btnFindExpensiveToys_Click(object sender, EventArgs e)
+		{
+			// 2) Find names of the most expensive toys (within 1 gryvnia difference)
+			var mostExpensiveToys = toys.GroupBy(t => t.Price)
+										.OrderByDescending(group => group.Key)
+										.FirstOrDefault();
+
+			// Display results
+			MessageBox.Show($"Names of the most expensive toys:\n\n{string.Join("\n", mostExpensiveToys)}", "Results", MessageBoxButtons.OK, MessageBoxIcon.Information);
+		}
+
+		private void btnFindToyPrices_Click(object sender, EventArgs e)
+		{
+			// Validate and handle empty toy type input
+			if (string.IsNullOrEmpty(txtToyType.Text))
+			{
+				MessageBox.Show("Please enter a valid Toy Type.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				return;
+			}
+
+			// 3) Find prices of toys of a specific type
+			string toyType = txtToyType.Text.Trim();
+
+			var toyPrices = toys.Where(t => t.Name.Equals(toyType, StringComparison.OrdinalIgnoreCase))
+								.Select(t => t.Price);
+
+			// Display results
+			MessageBox.Show($"Prices of {toyType}:\n\n{string.Join("\n", toyPrices)}", "Results", MessageBoxButtons.OK, MessageBoxIcon.Information);
+		}
+	}
+
+	public class Toy
+	{
+		public string Name { get; set; }
+		public double Price { get; set; }
+		public string AgeRange { get; set; }
+
+		public Toy(string name, double price, string ageRange)
+		{
+			Name = name;
+			Price = price;
+			AgeRange = ageRange;
+		}
+
+		public override string ToString()
+		{
+			return $"{Name} - {Price} UAH - Age Range: {AgeRange}";
+		}
+
+		public bool IsAgeAppropriate(string ageGroup)
+		{
+			string[] ageBounds = ageGroup.Split('-');
+			if (ageBounds.Length == 2)
+			{
+				int minAge = int.Parse(ageBounds[0]);
+				int maxAge = int.Parse(ageBounds[1]);
+
+				int toyMinAge = int.Parse(AgeRange.Split('-')[0]);
+				int toyMaxAge = int.Parse(AgeRange.Split('-')[1]);
+
+				return minAge <= toyMinAge && maxAge >= toyMaxAge;
+			}
+			return false;
 		}
 	}
 }
